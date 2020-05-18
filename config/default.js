4281ad6fde0521c5855185a01344588bd1e3a241
Sicong Teng <steng2@sheffield.ac.uk>
2020-05-18 22:30:50 +0100

    app setup

--- config/default.js
+++ config/default.js
@@ -5,5 +5,5 @@ module.exports = {
         key: 'pet',
         maxAge: 2592000000,
     },
-    mongodb: 'mongodb+srv://team:team@teamproject-9em6o.mongodb.net/test?retryWrites=true&w=majority',
+    mongodb: '',
 }
