cfa8fd45c899036a89f44f6e064f4559f05fbd76
zhang.qijia@live.com <Zqj123456>
2020-05-19 15:48:07 +0100

    model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     surname: String,
     firstname: String,
     email: String,
