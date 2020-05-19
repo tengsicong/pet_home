922506b43f5127d574d04474f72a0661dcaf41d3
Sicong Teng <steng2@sheffield.ac.uk>
2020-05-19 14:38:25 +0100

    signin.js

---
+++ routes/signin.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/user_signin', function(req, res) {
+
+});
+
+router.post('/admin_signin', function(req, res) {
+
+});
