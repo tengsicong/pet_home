82d22fb872e590b33eb904fde1a7290fbc90bdc8
NEdgeWeb <45887603+NEdgeWeb@users.noreply.github.com>
2020-05-28 01:08:24 +0100

    Nicky Work

--- public/javascripts/animal_detail.js
+++ public/javascripts/animal_detail.js
@@ -1,11 +1,8 @@
-/**
- * Show evaluation Modal
- *
- * - To be implemented -
- */
-$('#adopt button').click(function () {
-    alert("hello");
-    //let modal = $('#modal-lg-applyForAdopt');
-    //modal.find("span.prompt_message").text("Please fill out the below form with current information..");
-    //modal.modal();
+
+$(document).ready(function () {
+    $('#adopt button').click(function() {
+        let modal = $('#modal-lg-applyForAdopt');
+        modal.find("span.prompt_message").text("Please fill out the below form with current information..");
+        modal.modal();
+    });
 });
